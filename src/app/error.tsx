9d98782e9d78bf9ee7f13ec1a2080498c1413354
Collin Dunphy <cpdun03@gmail.com>
2023-06-25 21:37:03 -0400

    Update Error Page

---
+++ src/app/error.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return <h2>Something went wrong!</h2>
+}
