f5ada4a59663ea44f751470f420d9fa1e2c9a276
Collin Dunphy <cpdun03@gmail.com>
2023-07-02 12:00:04 -0400

    Add profile image and update title text on `/index`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import avatar from 'app/avatar.jpg'
+
 export default function Page() {
   return (
     <>
       <div className="flex-start flex-col p-6">
-        <div className="text-xl font-medium">Hey, I&apos;m Collin 👋</div>
+        <Link href="/">
+          <Image
+            className="h-16 w-16 rounded-full border border-neutral-200 dark:border-neutral-700"
+            src={avatar}
+            alt="Collin Dunphy headshot"
+            height={64}
+            width={64}
+            sizes="33vw"
+            style={{ objectFit: 'cover' }}
+          />
+        </Link>
+
+        <div className="text-4xl font-bold">
+          Software engineer, amazing human, and ameture comedian.
+        </div>
         <p className="text-slate-500">
-          I&apos;m a Junior at Virginia Tech majoring in Computer Science.
+          Hey, I&apos;m Collin. 👋 I&apos;m a Junior majoring in Computer
+          Science at Virginia Tech.
         </p>
       </div>
     </>
