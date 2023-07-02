b2c790e78b698b25da8aea0d1cbe759713fc8e95
Collin Dunphy <cpdun03@gmail.com>
2023-07-01 22:20:34 -0400

    Add SEC Redirect and Update Home Page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/secatvt',
+        destination: 'https://sec.vt.edu/collin',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Link from 'next/link'
-
 export default function Page() {
   return (
     <>
       <div className="flex-start flex-col p-6">
-        <div className="text-xl font-medium">Hey, I'm Collin 👋</div>
+        <div className="text-xl font-medium">Hey, I&apos;m Collin 👋</div>
         <p className="text-slate-500">
-          I'm a Junior at Virginia Tech majoring in Computer Science.
+          I&apos;m a Junior at Virginia Tech majoring in Computer Science.
         </p>
       </div>
     </>
