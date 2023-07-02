71dac4742f3d1bebff6ed14eead26dbd20825c2f
Collin Dunphy <cpdun03@gmail.com>
2023-07-02 11:59:26 -0400

    Add sample text to `/about`

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,34 @@
+import Link from 'next/link'
+
 export default function Page() {
-  return <p>Hello World my name is Collin Dunphy</p>
+  return (
+    <p>
+      As a student leader with an insatiable appetite for learning new
+      technology and business expertise, I am an aspiring computer scientist set
+      to graduate from{' '}
+      <Link target="_blank" href="https://vt.edu" rel="noreferrer">
+        Virginia Tech
+      </Link>{' '}
+      in May of 2025. I lead and serve my community as an Eagle Scout and as a
+      directing member of the{' '}
+      <Link target="_blank" href="https://sec.vt.edu" rel="noreferrer">
+        Student Engineers&apos; Council at Virginia Tech
+      </Link>
+      , I oversee a 501(c)(3) nonprofit with $800k+ in annual revenue, manage a
+      team of 20+ student engineers, and work with the university to improve the
+      student experience. I am a results-driven mobile application developer
+      with four years of experience in Swift (iOS), Flutter, and React. One of
+      my notable projects is an app designed to assist Speech Pathologists with
+      their clinical assessments, receiving over 800 downloads in its first
+      week. I also have experience coding in Java and Python, as well as
+      deploying application backend on AWS and Google Firebase. Additionally, I
+      have hands-on expertise in 3D prototyping, robotics design and
+      fabrication, and Computer-Aided Design (CAD). Most recently I had
+      internship last summer working on mobile SDK for a fraud detection and
+      analysis product. I am now doing research for the VT computer science
+      department on their CSGenome project, which researches and catalogs
+      different computer systems. I am currently looking for an internship or
+      contract software engineering position starting in May of 2023.
+    </p>
+  )
 }
