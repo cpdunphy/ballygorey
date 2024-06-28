fd4aeccee393157cf72281d15ffb03c3ace524c9
Collin Dunphy <cpdun03@gmail.com>
2024-06-28 10:23:52 -0400

    Update Footer Copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <hr className="my-4 border-gray-200 sm:mx-auto lg:my-6 dark:border-gray-800" />
       <div className="mx-auto max-w-7xl px-6 pb-6 sm:flex sm:items-center sm:justify-between">
         <p className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          &copy; 2023 Ballygorey Labs LLC. All Rights Reserved.
+          &copy; 2024 Ballygorey Labs. All Rights Reserved.
         </p>
         <Socials />
       </div>
