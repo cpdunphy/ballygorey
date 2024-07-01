d07bfc02e772d68db059f8dc69695d8d07c25e12
Collin Dunphy <cpdun03@gmail.com>
2024-07-01 14:47:09 -0400

    Update `Badge` to `Chip` and add dynamically colored background

---
+++ src/components/chip.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+interface BadgeProps {
+  color?: string
+  children: React.ReactNode
+}
+
+export default function Badge({ color, children }: BadgeProps) {
+  function adjustBrightness(
+    hex: string,
+    percent: number,
+    opacity: number
+  ): string {
+    let r = parseInt(hex.slice(1, 3), 16)
+    let g = parseInt(hex.slice(3, 5), 16)
+    let b = parseInt(hex.slice(5, 7), 16)
+
+    r = Math.round(Math.min(255, Math.max(0, r + (255 * percent) / 100)))
+    g = Math.round(Math.min(255, Math.max(0, g + (255 * percent) / 100)))
+    b = Math.round(Math.min(255, Math.max(0, b + (255 * percent) / 100)))
+
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`
+  }
+
+  function getLightAndDarkVariants(hex: string): [string, string] {
+    const lightVariant = adjustBrightness(hex, 30, 0.2) // Increase brightness by 30%
+    const darkVariant = adjustBrightness(hex, -20, 1.0) // Decrease brightness by 30%
+    return [lightVariant, darkVariant]
+  }
+
+  const [lightVariant, darkVariant] = color
+    ? getLightAndDarkVariants(color)
+    : []
+
+  return (
+    <div
+      className={`mt-2 rounded-full bg-gray-400/20 px-3 py-1 text-xs font-medium text-gray-700 dark:text-gray-300`}
+      style={{
+        backgroundColor: lightVariant,
+        color: color,
+      }}
+    >
+      {children}
+    </div>
+  )
+}
