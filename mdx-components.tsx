938ded4f160ece0d74b02fdb421f2139b5074531
Collin Dunphy <cpdun03@gmail.com>
2024-12-25 22:38:40 -0400

    Move `mdx-components.tsx` to root

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,9 @@ import type { MDXComponents } from 'mdx/types'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    // h1: ({ children }) => (
+    //   <h1 style={{ color: 'red', fontSize: '48px' }}>{children}</h1>
+    // ),
     ...components,
   }
 }
